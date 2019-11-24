90d4e515f4e58998dff77367559607454532a238
ligain <khokhlov86@gmail.com>
2019-11-24 19:10:45 +0200

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos/8"
+  config.vm.box_version = "1905.1"
+  config.vm.provision "shell", inline: <<-SHELL
+
+  # Setup mail agent
+  dnf install postfix -y
+  systemctl enable postfix
+  systemctl start postfix
+  dnf install mailx -y
+
+  cp /vagrant/logwatch.sh /etc/cron.hourly/
+  chmod +x /etc/cron.hourly/logwatch.sh
+  SHELL
+end
---
+++ logwatch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+lockfile=/tmp/logwatch
+
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null;
+then
+    trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+    while true
+    do
+        # What to do
+        ls -ld ${lockfile}
+        sleep 1
+    done
+   rm -f "$lockfile"
+   trap - INT TERM EXIT
+else
+   echo "Failed to acquire lockfile: $lockfile."
+   echo "Held by $(cat $lockfile)"
+fi
